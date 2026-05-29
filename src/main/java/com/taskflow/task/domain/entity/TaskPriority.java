f0080e600a2159c8171edda770a903a4ff2f4c55
davidAST <davidad5522@gmail.com>
2026-05-29 19:52:16 +0200

    Created the task entity

---
+++ src/main/java/com/taskflow/task/domain/entity/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.taskflow.task.domain.entity;
+
+public enum TaskPriority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ src/main/java/com/taskflow/task/domain/entity/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.taskflow.task.domain.entity;
+
+public enum TaskStatus {
+    OPEN,
+    COMPLETE
+}
